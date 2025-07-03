a6a0bd1ae974a38608dc5b440ccef3ef0d504ec3
F4tsk4l <enockkip11@gmail.com>
2025-07-03 22:57:03 +0300

    Nvim update added

--- functions/update-all.fish
+++ functions/update-all.fish
@@ -1,26 +1,28 @@
 function upgrade-all
     function show_help
         echo "Usage:"
-        echo "  upgrade-all -A    Perform full upgrade (Pacman, Yay, Ya(Yazi), Fisher, Cargo, Pipx)"
-        echo "  upgrade-all -a    Perform partial upgrade (Ya(Yazi), Fisher, Cargo, Pipx)"
+        echo "  upgrade-all -A    Perform full upgrade (Pacman, Yay, Ya(Yazi), Fisher, Cargo, Nvim_Plugins, Pipx)"
+        echo "  upgrade-all -a    Perform partial upgrade (Ya(Yazi), Fisher, Cargo, Nvim_Plugins, Pipx)"
         echo "  upgrade-all -h    Display this help message"
     end
 
     if set -q argv[1]
         switch "$argv[1]"
             case -A
-                echo "Full upgrade selected (Pacman, Yay, Ya(Yazi), Fisher, Cargo, Pipx)"
+                echo "Full upgrade selected (Pacman, Yay, Ya(Yazi), Fisher, Cargo, Nvim_Plugins, Pipx)"
                 sudo pacman -Syud --noconfirm
                 and yay -a -Su --noconfirm
                 and ya pkg upgrade
                 and fisher update
+                and nvim --headless "+Lazy! update" +qa
                 and cargo install-update -a
                 and pipx upgrade-all
             case -a
-                echo "Partial upgrade selected (Ya(Yazi), Fisher, Cargo, Pipx)"
+                echo "Partial upgrade selected (Ya(Yazi), Fisher, Cargo, Nvim_Plugins, Pipx)"
                 ya pkg upgrade
                 and fisher update
                 and cargo install-update -a
+                and nvim --headless "+Lazy! update" +qa
                 and pipx upgrade-all
             case -h
                 show_help
